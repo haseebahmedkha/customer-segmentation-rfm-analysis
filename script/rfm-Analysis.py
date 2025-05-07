60f5b83bf96d6a129336685bc578a0700ab0284c
Haseeb-Ahmed-QA-Eng <haseebahmed434@gmail.com>
2025-05-07 15:20:20 +0500

    necessary Libraries Added Successfully

---
+++ script/rfm-Analysis.py
@@ -0,0 +1,13 @@
+# importing necessary libraries
+import pandas as pd
+import pymysql
+from sqlalchemy import create_engine
+import datetime
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import mysql.connector
+import plotly.express as px
+
+
+
